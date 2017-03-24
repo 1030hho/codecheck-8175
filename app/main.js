3cabbeecd42c4cc53d93e751276518d89505266a
Emi Miyamoto <emi@Emis-MacBook-Pro.local>
2017-03-24 13:07:05 +0900

    clean

--- app/main.js
+++ app/main.js
@@ -7,14 +7,15 @@ function main(argv) {
   var coin = argv[0].split(',').map(v => Number(v));// array
   var sum = Number(argv[1]);
   var max = 501;
-  // console.log(coin, sum);
   if (sum > 500 || sum < 1) {
-    // console.log('invalid');
+    console.log(-1);
+    return -1;
   }
 
   coin.forEach(v => {
     if (v > 100 || v < 1) {
-      //// console.log('invalid');
+      console.log(-1);
+      return -1;
     }
   });
 
@@ -25,21 +26,18 @@ function main(argv) {
   for(var i=1; i < sum+1; i++) {
     t[i] = max;
   }
-  // // console.log('tlength', t.length);
   for (var i = 0; i < coin.length; i++) {
 
     for (var j = coin[i]; j < sum+1; j++) {
-      // console.log(i, j, t[j], t[j - coin[i]] + 1);
       t[j] = Math.min(t[j], t[j - coin[i]] + 1);
     }
   }
-// console.log(t);
-  // console.log(t[sum]);
   if (t[sum] == max) {
-    // console.log('cant solve');
     console.log(-1);
+    return -1;
   }
   console.log(t[sum]);
+  return t[sum];
 }
 
 module.exports = main;
